d00d322d35a2abbcaa411808e25fc5612812bca6
Hassan <hassan.rachid5@gmail.com>
2020-05-23 18:51:45 -0400

    port fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.set('port', 4825);
+var port = process.env.PORT || 4825
 
 var path = require('path');
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
@@ -26,7 +26,14 @@ app.use((req, res) => {
 	res.render('404');
 });
 
-app.listen(app.get('port'), function() {
-	console.log('Connection established. Ctrl-C to terminate...');
+app.use((err, req, res, next) => {
+	console.error(err.stack);
+	res.type('plain/text');
+	res.status(500);
+	res.render('500');
+})
+
+app.listen(port, function() {
+	console.log('Connection established on port ' + port + '. Ctrl-C to terminate...');
 });
 
